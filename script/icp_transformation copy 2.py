68ae7411855ed88cbac61c859d8db36ea3cf6695
retia <regame0804@gmail.com>
2024-03-22 18:44:43 +0900

    change little

---
+++ script/icp_transformation copy 2.py
@@ -0,0 +1,34 @@
+import numpy as np
+import open3d as o3d
+
+def icp_transformation(pcd1, pcd2):
+    threshold = 0.02
+    reg_p2p = o3d.pipelines.registration.registration_icp(
+        pcd1, pcd2, threshold, np.eye(4),
+        o3d.pipelines.registration.TransformationEstimationPointToPoint()
+    )
+    
+    return reg_p2p
+    
+
+if __name__ == "__main__":
+    bin_path = "./dataset/KITTI/00/velodyne"
+    pcd_path = "./dataset/KITTI/00/velodyne_pcd"
+
+    pcd1 = o3d.io.read_point_cloud(pcd_path+"/000000.pcd")
+    pcd2 = o3d.io.read_point_cloud(pcd_path+"/000001.pcd")
+
+
+    reg_p2p = icp_transformation(pcd1, pcd2)
+
+    print("Transformation is:")
+    print(reg_p2p.transformation)
+
+    # 회전 및 이동 추출
+    rotation_matrix = reg_p2p.transformation[:3, :3]
+    translation_vector = reg_p2p.transformation[:3, 3]
+    print("Rotation matrix:")
+    print(rotation_matrix)
+    print("Translation vector:")
+    print(translation_vector)
+    
