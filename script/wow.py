ccab89cb3d5f1083fe809c8e2eba357c1f5b134f
retia <regame0804@gmail.com>
2024-03-22 19:59:00 +0900

    change something

--- script/wow.py
+++ script/wow.py
@@ -3,26 +3,16 @@ import open3d as o3d
 import os
 import copy
 
-def find_transformation(source, target, trans_init):
-    threshold = 0.2
-    transformation = o3d.pipelines.registration.registration_icp(
-        source, target, threshold, trans_init,
-        o3d.pipelines.registration.TransformationEstimationPointToPlane()
-    ).transformation
-    
-    return transformation
-
-def get_pcd_from_numpy(pcd_np):
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(pcd_np[:, :3])
-    return pcd
-
 class loaderKITTI:
     
     def __init__(self, dataset_path):
         self.folder_path = dataset_path
         self.pcd_list = os.listdir(self.folder_path)
         self.pcd_list.sort()
+        
+        self.down_sampling_flag = False
+        self.delete_road_flag = False
+        self.remove_outlier_flag = False
     
     def length(self):
         return len(self.pcd_list)
@@ -30,7 +20,35 @@ class loaderKITTI:
     def get_item(self, ind):
         path = os.path.join(self.folder_path, self.pcd_list[ind])
         pcd = o3d.io.read_point_cloud(path)
+        
+        if self.down_sampling_flag:
+            pcd = pcd.voxel_down_sample(voxel_size=0.2)
+        
+        if self.remove_outlier_flag:
+            pcd = self.remove_outliers(pcd)
+        
+        if self.delete_road_flag:
+            pcd = self.only_objects(pcd)
+            
         return pcd
+    
+    def only_objects(self, pcd):
+        plane, inliers = pcd.segment_plane(distance_threshold=0.3, ransac_n=3, num_iterations=100)
+        objects = pcd.select_by_index(inliers, invert=True)
+        return objects
+    
+    def remove_outliers(self, pcd):
+        pcd, inliers = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+        return pcd
+
+    def down_sampling(self, voxel_size=0.2):
+        self.voxel_down = True
+    
+    def delete_road(self):
+        self.delete_road_flag = True
+    
+    def remove_outlier(self):
+        self.remove_outlier_flag = True
       
 
 class OdometryEstimator:
@@ -44,12 +62,12 @@ class OdometryEstimator:
         # target.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
         
         transformation = o3d.pipelines.registration.registration_icp(
-            source, target, self.threshold, self.last_position,
+            source, target, self.threshold, np.eye(4),
             o3d.pipelines.registration.TransformationEstimationPointToPoint()
             # o3d.pipelines.registration.TransformationEstimationPointToPlane()
         ).transformation
         
-        # self.last_position = transformation @ self.last_position
+        self.last_position = self.last_position @ transformation
 
         return transformation
     
@@ -76,41 +94,41 @@ class Car:
         self.last_position = self.last_position @ transformation
         self.position_list.append(self.last_position)
 
-def remove_outliers(pcd):
-    pcd, inliers = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
-    return pcd
-
-def only_objects(pcd):
-    plane, inliers = pcd.segment_plane(distance_threshold=0.3, ransac_n=3, num_iterations=100)
-    objects = pcd.select_by_index(inliers, invert=True)
-    return objects
 
 if __name__ == "__main__":
     dataset_path = "./dataset/KITTI/00/velodyne_pcd"
     loader = loaderKITTI(dataset_path)
+    loader.down_sampling(voxel_size=0.2)
+    loader.delete_road()
+    loader.remove_outlier()
+    
+    source = loader.get_item(0)
+    target = loader.get_item(0)
+    
     odometry = OdometryEstimator()
-    
     car = Car()
-    source = loader.get_item(0).voxel_down_sample(voxel_size=0.2)
-    target = loader.get_item(1).voxel_down_sample(voxel_size=0.2)
     
-    source.paint_uniform_color([1,0,0])
-    target.paint_uniform_color([0,0,1])
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(target)
+    vis.add_geometry(car.get_car())
     
-    # 이상치 제거
-    source = remove_outliers(source)
-    target = remove_outliers(target)
-    
-    # 도로 제거
-    source = only_objects(source)
-    target = only_objects(target)
-    
-    # ICP 매칭
-    # ori = copy.deepcopy(source).paint_uniform_color([0,1,0])
-    car_ori = copy.deepcopy(car.get_car())
-    for _ in range(10):
+    for i in range(loader.length()):
+        source.points = target.points
+        target.points = loader.get_item(i).points
+        
         transformation = odometry.transformation(source, target)
+        
         car.transform(transformation)
-        source.transform(transformation)
-        o3d.visualization.draw_geometries([car_ori, car.get_car(), source, target])
+        vis.add_geometry(car.get_car(clone=True))
+        target.transform(odometry.last_position)
+        
+        vis.update_geometry(target)
+        # vis.update_geometry(car.get_car())
+        vis.poll_events()
+        vis.update_renderer()
+    
+    vis.destroy_window()
+        
+        
     
